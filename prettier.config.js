7d6184d6ba3bc2f2ce831df7ad1e3c46e2e2e244
swimmingone <dev.swimmingone@gmail.com>
2022-05-22 00:26:23 +0900

    feat: set up lint, formatter

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  singleQuote: true,
-  semi: false,
+    "singleQuote": true,
+    "parser": "typescript",
+    "trailingComma": "all",
+    "semi": true,
+    "useTabs": true,
+    "tabWidth": 4,
+    "printWidth": 100,
+    "endOfLine": "lf",
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": false,
+    "proseWrap": "preserve",
+    "quoteProps": "as-needed",
+    "arrowParens": "always",
+    "bracketSpacing": true
 }
