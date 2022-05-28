89d99a0b10770c18c8b7c03b4c998316ff6f55e3
swimmingone <dev.swimmingone@gmail.com>
2022-05-28 15:04:35 +0900

    style: todo item 스타일 수정

--- components/TodoList/TodoItem.tsx
+++ components/TodoList/TodoItem.tsx
@@ -9,21 +9,21 @@ const TodoItem = ({ todo }: Props) => {
 	let { title, isDone, tags } = todo;
 	return (
 		<div className={'box-border border-t p-4'}>
-			<div className={'flex justify-between'}>
+			<div className={'flex'}>
 				<input
 					type={'checkbox'}
 					className={'checkbox checkbox-primary checkbox-lg'}
 					checked={isDone}
 				/>
-				<div className={'flex w-11/12 items-center hover:text-primary'}>
+				<div className={'flex w-11/12 cursor-pointer hover:text-primary'}>
 					<div className={'flex-grow-1 pl-8 text-xl'}>{title}</div>
 					{/*{isUrgent && <div className={'badge badge-sm badge-warning'}>!</div>}*/}
 				</div>
 				<button className={'btn btn-ghost btn-circle btn-sm'}>x</button>
 			</div>
-			<div className={'box-border pl-16'}>
+			<div className={'box-border px-16'}>
 				{tags.map((tag) => (
-					<div key={tag.name} className={`badge badge-${tag.color}`}>
+					<div key={tag.name} className={`badge badge-${tag.color} mr-1`}>
 						{tag.name}
 					</div>
 				))}
