4762079aaf5536a0b9cfc7739f2fba26ed06d9a2
swimmingone <dev.swimmingone@gmail.com>
2022-05-28 15:09:24 +0900

    feat: 할 일 생성 페이지 뷰

--- components/CreateForm/CreateForm.tsx
+++ components/CreateForm/CreateForm.tsx
@@ -1,20 +1,67 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import FormItem from './FormItem';
 import SubmitButton from '../SubmitButton';
+import useInputs from '../../hooks/useInputs';
+import { useSetRecoilState } from 'recoil';
+import { todoListState } from '../../store/TodoListState';
 
 const CreateForm = () => {
+	const router = useRouter();
+	const setTodoList = useSetRecoilState(todoListState);
+
+	const [data, onChangeData] = useInputs({
+		title: '',
+		description: '',
+	});
+
+	const newID = function () {
+		return Math.random().toString(36).substring(2, 16);
+	};
+	const addTodo = () => {
+		setTodoList((oldList) => [
+			...oldList,
+			{
+				id: newID(),
+				title: data.title,
+				description: data.description,
+				tags: [],
+				dueDate: 'string',
+				creationDate: 'string',
+				editDate: 'string',
+				doneDate: 'string',
+				isDone: false,
+			},
+		]);
+	};
+
+	const onClickSubmit = () => {
+		addTodo();
+		router.push('/');
+	};
+
 	return (
 		<div className={'box-border flex w-full flex-col items-center justify-between gap-4 p-4'}>
 			<FormItem label={'할 일'}>
-				<input type={'text'} className="input input-bordered input-sm" />
+				<input
+					name={'title'}
+					type={'text'}
+					className="input input-bordered input-sm"
+					onChange={onChangeData}
+				/>
 			</FormItem>
 			<FormItem label={'상세설명'}>
-				<textarea rows={4} className="textarea textarea-bordered w-full" />
+				<textarea
+					name={'description'}
+					rows={4}
+					className="textarea textarea-bordered w-full"
+					onChange={onChangeData}
+				/>
 			</FormItem>
 			<FormItem label={'마감목표일'}>
 				<input type={'date'} className="input input-bordered input-sm w-full" />
 			</FormItem>
-			<SubmitButton />
+			<SubmitButton onSubmit={onClickSubmit} />
 		</div>
 	);
 };
