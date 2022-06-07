9703c9b9ff61c4f5e8f04b94490310ab532bde60
swimmingone <dev.swimmingone@gmail.com>
2022-06-08 00:24:14 +0900

    feat: 수정 폼에서 태그 추가

--- src/todo/components/templates/EditForm.tsx
+++ src/todo/components/templates/EditForm.tsx
@@ -6,6 +6,9 @@ import SubmitButton from '../molecules/SubmitButton';
 import { Todo } from '../../types/Todo';
 import dayjs from 'dayjs';
 import useUnloadAlert from '../../../hooks/useUnloadAlert';
+import TagColorRadio from '../molecules/TagColorRadio';
+import TagList from '../organisms/TagList';
+import { Tag, TagColor } from '../../types/Tag';
 
 interface Prop {
 	selectedTodo: Todo;
@@ -17,9 +20,42 @@ const EditForm = ({ selectedTodo }: Prop) => {
 	const { enablePrevent, disablePrevent } = useUnloadAlert();
 
 	const [data, setData] = useState(selectedTodo);
+	const [tags, setTags] = useState<Tag[]>(selectedTodo.tags);
+	const [tagNameInput, setTagNameInput] = useState('');
+	const [tagColor, setTagColor] = useState<TagColor>(null);
+
+	const isValidTag = (newTag: Tag) => {
+		let isValid = true;
+		if (tags.filter((tag) => tag.name === newTag.name).length) {
+			alert('같은 이름의 태그가 존재합니다.');
+			isValid = false;
+		}
+		if (tagColor === null) {
+			alert('태그 색상을 선택하지 않았습니다.');
+			isValid = false;
+		}
+		if (!tagNameInput) {
+			alert('태그 이름이 입력되지 않았습니다.');
+			isValid = false;
+		}
+		return isValid;
+	};
+
+	const addTag = (newTag: Tag) => {
+		if (!isValidTag(newTag)) return;
+		else setTags([...tags, newTag]);
+	};
+
+	const onTagEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			const newTag = { name: tagNameInput, color: tagColor };
+			addTag(newTag);
+			setTagNameInput('');
+		}
+	};
 
 	const onClickSubmit = () => {
-		editTodo({ ...data, editDate: dayjs().format('YYYY/MM/DD hh:mm') });
+		editTodo({ ...data, editDate: dayjs().format('YYYY/MM/DD hh:mm'), tags: tags });
 		router.push('/');
 	};
 
@@ -50,6 +86,23 @@ const EditForm = ({ selectedTodo }: Prop) => {
 						<p className={'text-sm text-red-600'}>*필수 입력 항목입니다.</p>
 					)}
 				</>
+			</FormItem>
+			<FormItem label={'태그'}>
+				<div className={'flex flex-col gap-2'}>
+					<TagColorRadio setTagColor={setTagColor} />
+					<input
+						name={'tag'}
+						type={'text'}
+						className="input input-bordered input-sm"
+						value={tagNameInput}
+						onChange={(e) => setTagNameInput(e.target.value)}
+						onKeyPress={(e) => onTagEnter(e)}
+					/>
+					<p className={'text-sm'}>
+						*색상을 선택하고 Enter 키를 입력하면 태그가 생성됩니다.
+					</p>
+					<TagList tags={tags} />
+				</div>
 			</FormItem>
 			<FormItem label={'상세설명'}>
 				<textarea
