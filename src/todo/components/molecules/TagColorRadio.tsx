a6032d9648f947356630e6b389e421b7ea7dda7d
swimmingone <dev.swimmingone@gmail.com>
2022-06-08 01:49:56 +0900

    feat: 태그 색상 선택 예시 추가

--- src/todo/components/molecules/TagColorRadio.tsx
+++ src/todo/components/molecules/TagColorRadio.tsx
@@ -10,37 +10,46 @@ const TagColorRadio = ({ setTagColor }: Props) => {
 	};
 
 	return (
-		<div className={'flex gap-2'}>
-			<input
-				type="radio"
-				name="tag-color-radio"
-				className="radio border-neutral"
-				onClick={() => onClickButton('')}
-			/>
-			<input
-				type="radio"
-				name="tag-color-radio"
-				className="radio border-info checked:bg-info"
-				onClick={() => onClickButton('info')}
-			/>
-			<input
-				type="radio"
-				name="tag-color-radio"
-				className="radio border-warning checked:bg-warning"
-				onClick={() => onClickButton('warning')}
-			/>
-			<input
-				type="radio"
-				name="tag-color-radio"
-				className="radio border-success checked:bg-success"
-				onClick={() => onClickButton('success')}
-			/>
-			<input
-				type="radio"
-				name="tag-color-radio"
-				className="radio border-error checked:bg-error"
-				onClick={() => onClickButton('error')}
-			/>
+		<div className={'flex w-fit flex-col items-center gap-2'}>
+			<div className={'flex gap-2'}>
+				<div className={`badge`}></div>
+				<div className={`badge badge-info`}></div>
+				<div className={`badge badge-warning`}></div>
+				<div className={`badge badge-success`}></div>
+				<div className={`badge badge-error`}></div>
+			</div>
+			<div className={'flex gap-1'}>
+				<input
+					type="radio"
+					name="tag-color-radio"
+					className="radio border-neutral"
+					onClick={() => onClickButton('')}
+				/>
+				<input
+					type="radio"
+					name="tag-color-radio"
+					className="radio border-info checked:bg-info"
+					onClick={() => onClickButton('info')}
+				/>
+				<input
+					type="radio"
+					name="tag-color-radio"
+					className="radio border-warning checked:bg-warning"
+					onClick={() => onClickButton('warning')}
+				/>
+				<input
+					type="radio"
+					name="tag-color-radio"
+					className="radio border-success checked:bg-success"
+					onClick={() => onClickButton('success')}
+				/>
+				<input
+					type="radio"
+					name="tag-color-radio"
+					className="radio border-error checked:bg-error"
+					onClick={() => onClickButton('error')}
+				/>
+			</div>
 		</div>
 	);
 };
