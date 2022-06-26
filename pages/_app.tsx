1ccf7fdc072d410bf41296857133977b3c1457eb
swimmingone <dev.swimmingone@gmail.com>
2022-06-27 00:07:34 +0900

    refactor: context 관련 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,14 @@ import type { AppProps } from 'next/app';
 import '../src/common/styles/globals.css';
 import Layout from '../src/common/components/Layout';
 import { RecoilRoot } from 'recoil';
-import TodoContext from '../src/todo/store/todoContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<TodoContext.Provider value={[]}>
-			<RecoilRoot>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</RecoilRoot>
-		</TodoContext.Provider>
+		<RecoilRoot>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</RecoilRoot>
 	);
 }
 
