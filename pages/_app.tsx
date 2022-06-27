6e42e37aeb8753bce06d9ce4ef4a585176b5e1de
swimmingone <dev.swimmingone@gmail.com>
2022-06-27 10:58:07 +0900

    refactor: recoil 의존성 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../src/common/styles/globals.css';
 import Layout from '../src/common/components/Layout';
-import { RecoilRoot } from 'recoil';
 import TodoProvider from '../src/todo/provider/TodoProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<TodoProvider>
-			<RecoilRoot>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</RecoilRoot>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</TodoProvider>
 	);
 }
