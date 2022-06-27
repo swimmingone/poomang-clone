ba7cd5ec98d685cea85ae01b71d4fca127805580
swimmingone <dev.swimmingone@gmail.com>
2022-06-27 09:17:25 +0900

    refactor: todo reducer 분리

--- src/todo/provider/TodoProvider.tsx
+++ src/todo/provider/TodoProvider.tsx
@@ -6,69 +6,10 @@ import React, {
 	useEffect,
 	useReducer,
 } from 'react';
+import TodoReducer from './TodoReducer';
 import { Todo } from '../types/Todo';
-import dayjs from 'dayjs';
-import newID from '../../utils/newId';
-import removeItemAtIndex from '../../utils/removeItemAtIndex';
-import replaceItemAtIndex from '../../utils/replaceItemAtIndex';
-
-type UseTodosAction = {
-	type: string;
-	id?: string;
-	payload?: Todo;
-	value?: Todo[];
-};
 
 const initialState: Todo[] = [];
-
-function reducer(state: Todo[], action: UseTodosAction): Todo[] {
-	const { type, id, payload, value } = action;
-	const targetIndex = state.findIndex((todo) => todo.id === id);
-	const targetTodo = state.find((todo) => todo.id === id);
-
-	switch (type) {
-		case 'INIT_STORED':
-			if (!value) return [];
-			return value;
-
-		case 'TOGGLE_DONE':
-			if (!targetTodo) return state;
-			return replaceItemAtIndex<Todo>(state, targetIndex, {
-				...targetTodo,
-				isDone: !targetTodo.isDone,
-			});
-
-		case 'CREATE_TODO':
-			if (!payload) return state;
-			console.log(payload);
-			return [
-				...state,
-				{
-					id: newID(),
-					title: payload.title,
-					description: payload.description,
-					tags: payload.tags,
-					dueDate: payload.dueDate,
-					creationDate: dayjs().format('YYYY/MM/DD hh:mm'),
-					editDate: '',
-					doneDate: '',
-					isDone: false,
-				},
-			];
-		case 'EDIT_TODO':
-			if (!targetTodo || !payload) return state;
-			return replaceItemAtIndex<Todo>(state, targetIndex, {
-				...payload,
-				editDate: dayjs().format('YYYY/MM/DD hh:mm'),
-			});
-		case 'DELETE_TODO':
-			return removeItemAtIndex<Todo>(state, targetIndex);
-		case 'DELETE_ALL_DONE':
-			return state.filter((todo) => !todo.isDone);
-		default:
-			return state;
-	}
-}
 
 interface TodoContextType {
 	todos: Todo[];
@@ -94,7 +35,7 @@ interface Prop {
 }
 
 const TodoProvider = ({ children }: Prop) => {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(TodoReducer, initialState);
 
 	const onToggle = useCallback((id: string) => {
 		dispatch({
---
+++ src/todo/provider/TodoReducer.ts
@@ -0,0 +1,61 @@
+import { Todo } from '../types/Todo';
+import replaceItemAtIndex from '../../utils/replaceItemAtIndex';
+import removeItemAtIndex from '../../utils/removeItemAtIndex';
+import newID from '../../utils/newId';
+import dayjs from 'dayjs';
+
+type UseTodosAction = {
+	type: string;
+	id?: string;
+	payload?: Todo;
+	value?: Todo[];
+};
+
+export default function reducer(state: Todo[], action: UseTodosAction): Todo[] {
+	const { type, id, payload, value } = action;
+	const targetIndex = state.findIndex((todo) => todo.id === id);
+	const targetTodo = state.find((todo) => todo.id === id);
+
+	switch (type) {
+		case 'INIT_STORED':
+			if (!value) return [];
+			return value;
+
+		case 'TOGGLE_DONE':
+			if (!targetTodo) return state;
+			return replaceItemAtIndex<Todo>(state, targetIndex, {
+				...targetTodo,
+				isDone: !targetTodo.isDone,
+			});
+
+		case 'CREATE_TODO':
+			if (!payload) return state;
+			console.log(payload);
+			return [
+				...state,
+				{
+					id: newID(),
+					title: payload.title,
+					description: payload.description,
+					tags: payload.tags,
+					dueDate: payload.dueDate,
+					creationDate: dayjs().format('YYYY/MM/DD hh:mm'),
+					editDate: '',
+					doneDate: '',
+					isDone: false,
+				},
+			];
+		case 'EDIT_TODO':
+			if (!targetTodo || !payload) return state;
+			return replaceItemAtIndex<Todo>(state, targetIndex, {
+				...payload,
+				editDate: dayjs().format('YYYY/MM/DD hh:mm'),
+			});
+		case 'DELETE_TODO':
+			return removeItemAtIndex<Todo>(state, targetIndex);
+		case 'DELETE_ALL_DONE':
+			return state.filter((todo) => !todo.isDone);
+		default:
+			return state;
+	}
+}
